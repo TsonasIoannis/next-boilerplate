d22c139fa02f71ee2c1d8624ecb884cb77bf8811
TsonasIoannis <32147385+TsonasIoannis@users.noreply.github.com>
2023-11-21 00:07:44 +0000

    build: use normal build output for cypress

--- next.config.js
+++ next.config.js
@@ -2,29 +2,23 @@
  * @type {import('next').NextConfig}
  */
 
+const isExport = process.env.BUILD_EXPORT || false;
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const isCypress = process.env.BUILD_CYPRESS || false;
 
 let nextConfig = {
   output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
 };
 
-if (isGithubActions) {
-  if (isCypress) {
-    nextConfig = {
-      output: undefined,
-    };
-  } else {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-    nextConfig = {
-      output: "export",
-      assetPrefix: `/${repo}/`,
-      basePath: `/${repo}`,
-      images: {
-        unoptimized: true,
-      },
-    };
-  }
+if (isExport && isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  nextConfig = {
+    output: "export",
+    assetPrefix: `/${repo}/`,
+    basePath: `/${repo}`,
+    images: {
+      unoptimized: true,
+    },
+  };
 }
 
 module.exports = nextConfig;
