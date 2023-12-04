39c50568ce6f41fc50e6e327d26375af5ae3d618
TsonasIoannis <32147385+TsonasIoannis@users.noreply.github.com>
2023-12-04 00:53:32 +0000

    feat: add mui dark mode detection

--- src/app/MainMenu.tsx
+++ src/app/MainMenu.tsx
@@ -7,10 +7,10 @@ import AntMainMenu from "./AntMainMenu";
 export default function MainMenu() {
   const { ui } = useUI();
   return (
-    <>
+    <div>
       {ui == "bootstrap" ? <BootstrapMainMenu /> : null}
       {ui == "mui" ? <MuiMainMenu /> : null}
       {ui == "ant" ? <AntMainMenu /> : null}
-    </>
+    </div>
   );
 }
